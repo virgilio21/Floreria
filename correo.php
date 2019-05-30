81dc3982873eb9a1d1124953111b314b9354e0df
Virgilio Padron Batun <virgilio.padron.sop.j@gmail.com>
2019-05-30 02:18:41 -0500

    ya

---
+++ correo.php
@@ -0,0 +1,14 @@
+<?php
+
+    $destino_correo = "virgilio.padron.sop.j@gmail.com";
+
+    $nombre = $_POST['nombre'];
+    $email = $_POST['correo'];
+    $telefono = $_POST['telefono'];
+    $texto = $_POST['area'];
+
+    $mensaje = "Nombre: " . $nombre . "\n" . "Correo: " . $email . "\n" . "telefono: " . $telefono . "\n" . "Mensaje: " . $texto;
+
+    mail($destino_correo, "Info", $mensaje);
+
+?>
